94bc26287177f7bf38297bb68b326761e45c8a43
jackiewaang <61704082+jackiewaang@users.noreply.github.com>
2024-08-06 01:23:05 +0200

    implemented ui version of rock paper scissors game

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+const line = document.querySelector("#line");
+
 function getComputerChoice(){
     const n = Math.floor(Math.random() * 4);
     if(n == 0){
@@ -9,51 +13,56 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    return prompt("Choose between Rock, Paper or Scissors").toLowerCase();
-}
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function playRound(humanChoice, computerChoice){
-        if(humanChoice == "rock" && computerChoice == "paper"){
-            console.log("You Lose! Paper beats Rock");
-            computerScore++;
-        } else if(humanChoice == "rock" && computerChoice == "scissors"){
-            console.log("You win! Rock beats Scissors");
-            humanScore++;
-        } else if(humanChoice == "paper" && computerChoice == "rock"){
-            console.log("You win! Paper beats Rock");
-            humanScore++;
-        } else if(humanChoice == "paper" && computerChoice == "scissors"){
-            console.log("You lose! Scissors beats Paper");
-            computerScore++;
-        } else if(humanChoice == "scissors" && computerChoice == "rock"){
-            console.log("You lose! Rock beats Scissors");
-            computerScore++;
-        } else if(humanChoice == "scissors" && computerChoice == "paper"){
-            console.log("You win! Scissors beats Paper");
-            humanScore++;
-        } else{
-            console.log("Draw");
-        }
-    }
-    
-    for(let i=0;i<5;i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        setTimeout(10000);
-    }
-    if(humanScore > computerScore){
-        console.log("You won");
-    } else if(humanScore < computerScore){
-        console.log("You lost");
+function playRound(humanChoice, computerChoice){
+    if(humanChoice == "rock" && computerChoice == "paper"){
+        line.textContent = "You Lose! Paper beats Rock";
+        computerScore++;
+        checkWinner();
+    } else if(humanChoice == "rock" && computerChoice == "scissors"){
+        line.textContent = "You win! Rock beats Scissors";
+        humanScore++;
+        checkWinner();
+    } else if(humanChoice == "paper" && computerChoice == "rock"){
+        line.textContent = "You win! Paper beats Rock";
+        humanScore++;
+        checkWinner();
+    } else if(humanChoice == "paper" && computerChoice == "scissors"){
+        line.textContent = "You lose! Scissors beats Paper";
+        computerScore++;
+        checkWinner();
+    } else if(humanChoice == "scissors" && computerChoice == "rock"){
+        line.textContent = "You lose! Rock beats Scissors";
+        computerScore++;
+        checkWinner();
+    } else if(humanChoice == "scissors" && computerChoice == "paper"){
+        line.textContent = "You win! Scissors beats Paper";
+        humanScore++;
+        checkWinner();
     } else{
-        console.log("Draw");
+        line.textContent = `Both players chose ${humanChoice}! Draw`;
     }
 }
 
-playGame();
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", function(e){
+    playRound(e.target.id, getComputerChoice());
+    display();
+}))
+
+const div = document.createElement("div");
+div.textContent = `You ${humanScore} - Bot ${computerScore}`;
+document.querySelector("body").appendChild(div);
+
+function display(){
+    div.textContent = `You ${humanScore} - Bot ${computerScore}`;
+}
+
+function checkWinner(){
+    if(humanScore == 5){
+        line.textContent = "You reached 5 points! You won";
+        buttons.forEach(button => button.replaceWith(button.cloneNode(true)));
+    } else if(computerScore == 5){
+        line.textContent = "The bot reached 5 points! You lose";
+        buttons.forEach(button => button.replaceWith(button.cloneNode(true)));
+    }
+}
